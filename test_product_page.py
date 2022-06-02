c5ec32e70416fcc3d38797f9f36afb6ab638d0c7
unknown <panov20@gmail.com>
2022-06-02 18:26:40 +1000

    product tests were added

--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,6 @@ def test_guest_can_add_product_to_basket_and_name_with_price_are_match(browser, 
     page.test_book_price_match_with_book_price_in_basket()
 
 
-@pytest.mark.new1
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -29,7 +28,6 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.new1
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
